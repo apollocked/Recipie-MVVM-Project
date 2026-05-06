017d229f0955ec073a2c0e9392dd7feb8095ae3b
apollocked <hamabarznji1990@gmail.com>
2026-05-06 19:50:09 +0300

    Add : save the theme inside users phone to call on load the app

--- lib/view_model/providers/theme_provider.dart
+++ lib/view_model/providers/theme_provider.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
   ThemeMode get themeMode => _themeMode;
+  static const String _themeKey = "theme_mode";
 
-  void setTheme(ThemeMode mode) {
-    _themeMode = mode;
-    notifyListeners();
+  ThemeProvider() {
+    _loadTheme();
   }
 
-  void toggleTheme() {
-    _themeMode = _themeMode == ThemeMode.light
-        ? ThemeMode.dark
-        : ThemeMode.light;
+  void setTheme(ThemeMode mode) async {
+    _themeMode = mode;
     notifyListeners();
+    await _saveTheme(mode);
+  }
+
+  void toggleTheme() async {
+    _themeMode = (_themeMode == ThemeMode.dark)
+        ? ThemeMode.light
+        : ThemeMode.dark;
+    notifyListeners();
+    await _saveTheme(_themeMode);
+  }
+
+  Future<void> _saveTheme(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_themeKey, mode.name);
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? themeName = prefs.getString(_themeKey);
+
+    if (themeName != null) {
+      _themeMode = ThemeMode.values.firstWhere(
+        (e) => e.name == themeName,
+        orElse: () => ThemeMode.system,
+      );
+      notifyListeners();
+    }
   }
 }
